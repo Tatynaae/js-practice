5a50c24b6cc6d0e5ead22a1542613a92a9caea9a
Tatyna <tatynaerkinbek@gmailcom>
2023-11-29 16:02:11 +0600

    practice

---
+++ array.js
@@ -0,0 +1,16 @@
+// function ArrayCreation() {
+  let arrayWithInitSize = new Array(4);
+  let arrayWithOneValue = new Array("5");
+  let arrayWithPreset = new Array(4, 5);
+  let arrayWithInitValue = new Array.of(4);
+  let arrayWithInitValues = new Array.of(4, 5, 6);
+  let simpleWayArray = ["a", "b", "c"];
+
+  console.log("\nArray creation:");
+  console.log("arrayWithInitSize:", arrayWithInitSize);
+  console.log("arrayWithOneValue:", arrayWithOneValue);
+  console.log("arrayWithPreset:", arrayWithPreset);
+  console.log("arrayWithInitValue:", arrayWithInitValue);
+  console.log("arrayWithInitValues:", arrayWithInitValues);
+  console.log("simpleWayArray:", simpleWayArray);
+// }
